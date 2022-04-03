d08f3de2a8174cdca1d6b662e8525bec0296fe8f
sacco raffaele <saccoraffaele@hotmail.com>
2022-04-03 14:15:22 +0200

    manca l'ultima traccia dell'esercizio

--- index.js
+++ index.js
@@ -6,21 +6,23 @@
 // 3. Ritrona solo il piÃ¹ alto per i Total Confirmed e il più basso dei Total Confirmed 
 
 async function covid19(){
-    const lista = document.querySelector('.lista')
+   
     const response = await fetch('https://api.covid19api.com/summary')
     const responsejson = await response.json();
     console.log(responsejson);
     const totalConfirmed = responsejson.Global.TotalConfirmed;
-    console.log(totalConfirmed);
+    // console.log(totalConfirmed);
     const country = responsejson.Countries.forEach(element => {
-       
-        let selezionaCountry = document.createElement('li')
-        selezionaCountry.innerHTML = `
+        const nazione = responsejson.Countries[0].Country
         
+        let selezionaCountry = document.createElement('li')
+        const lista = document.querySelector('.lista')
+        selezionaCountry.innerHTML = `
+            ${element.Country} ${element.CountryCode} ${element.TotalConfirmed}
         `
         lista.appendChild(selezionaCountry);
     });
-    console.log(country);
+    // console.log(country);
 }
 
 covid19()
